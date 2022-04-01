b64fc6041ada84fbcf35a60cb86f7a0a02ed86bd
elvecho <100934190+elvecho@users.noreply.github.com>
2022-04-01 15:08:50 +0200

    commit

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+string[] cibi = { "calamari fritti", "panino alla salamella", "pizza", "formaggio", "cereali" };
+Console.WriteLine("la lunghezza della dell array è: " + cibi.Length);
+
+for(int i = 0; i <= cibi.Length -1; i++)
+{
+    Console.WriteLine(cibi[i]);
+
+    
+
+}
+var mediana = cibi.Length / 2;
+if (mediana % 1 == 0)
+{
+    Console.WriteLine("la posizione mediana è: " + cibi[mediana]);
+}
+
+
+
+Console.WriteLine("prima posizione della classifica: " + cibi[0]);
+Console.WriteLine("ultima posizione della classifica: " + cibi[cibi.Length - 1]);
